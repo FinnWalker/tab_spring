bf6edffbeb8d80e072411025fe7b8ae28d6daddc
FinnWalker <finn3walker@gmail.com>
2019-10-25 05:23:09 +0100

    fdas

--- tab-spring.js
+++ tab-spring.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser);
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.use("/tab_spring/", express.static("public"));
