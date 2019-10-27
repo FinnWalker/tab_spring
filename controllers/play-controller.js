b72031870bdb7a0909b2a8ec08b372ed784adaba
FinnWalker <finn3walker@gmail.com>
2019-10-27 04:08:43 +0000

    standard commit

--- controllers/play-controller.js
+++ controllers/play-controller.js
@@ -3,6 +3,7 @@ const participantModel = require("../models/participant.js");
 
 module.exports = {
   emails: function(req, res) {
+    let emails = [];
     groupModel.findOne({ playing: true }, (err, group) => {
       if (err) {
         res.status(500).json({ message: "Error finding group" });
@@ -13,67 +14,47 @@ module.exports = {
             if (err) {
               res.status(500).json({ message: "Error finding participant" });
             } else if (participant) {
-              const email_0 = participant.email;
-              participantModel.findOne(
-                { _id: group.participant_1 },
-                (err, participant) => {
-                  if (err) {
-                    res
-                      .status(500)
-                      .json({ message: "Error finding participant" });
-                  } else if (participant) {
-                    const email_1 = participant.email;
+              emails.push(participant.email);
+            }
+            participantModel.findOne(
+              { _id: group.participant_1 },
+              (err, participant) => {
+                if (err) {
+                  res
+                    .status(500)
+                    .json({ message: "Error finding participant" });
+                } else if (participant) {
+                  emails.push(participant.email);
+                }
+                participantModel.findOne(
+                  { _id: group.participant_2 },
+                  (err, participant) => {
+                    if (err) {
+                      res
+                        .status(500)
+                        .json({ message: "Error finding participant" });
+                    } else if (participant) {
+                      emails.push(participant.email);
+                    }
                     participantModel.findOne(
-                      { _id: group.participant_2 },
+                      { _id: group.participant_3 },
                       (err, participant) => {
                         if (err) {
                           res
                             .status(500)
                             .json({ message: "Error finding participant" });
                         } else if (participant) {
-                          const email_2 = participant.email;
-                          participantModel.findOne(
-                            { _id: group.participant_3 },
-                            (err, participant) => {
-                              if (err) {
-                                res
-                                  .status(500)
-                                  .json({
-                                    message: "Error finding participant"
-                                  });
-                              } else if (participant) {
-                                const email_3 = participant.email;
-                                res.status(200).json({email_0, email_1, email_2, email_3});
-                              } else {
-                                res
-                                  .status(500)
-                                  .json({
-                                    message: "Error finding participant"
-                                  });
-                              }
-                            }
-                          );
-                        } else {
-                          res
-                            .status(500)
-                            .json({ message: "Error finding participant" });
+                          emails.push(participant.email);
                         }
+                        res.status(200).json(emails);
                       }
                     );
-                  } else {
-                    res
-                      .status(500)
-                      .json({ message: "Error finding participant" });
                   }
-                }
-              );
-            } else {
-              res.status(500).json({ message: "Error finding participant" });
-            }
+                );
+              }
+            );
           }
         );
-      } else {
-        res.status(200).json({ message: "No playing group found" });
       }
     });
   }
