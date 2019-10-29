52accacaa03846756f4b08ae416cb190e3f5c73b
FinnWalker <finn3walker@gmail.com>
2019-10-29 20:29:12 +0000

    standard

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,5 @@
 module.exports = {
-  scene_1: function(req, res) {
+  depthThreshold: function(req, res) {
     if(req.body.min && req.body.max) {
       res.status(200).json({ message: "Message sent" });
       req.app.io.emit("depth_threshold", {min: req.body.min, max: req.body.max});
